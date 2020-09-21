f33533228521569ca84fad1886c7c64293b64809
junpeitanaka <69292110+junpeitanaka@users.noreply.github.com>
2020-09-22 08:14:58 +0900

    フロント実装の準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'looks/index'
+  root to: "looks#index"
 end
