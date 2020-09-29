71a0e65f12d83c01676b942c9e6a1d50e1d5a255
junpeitanaka <69292110+junpeitanaka@users.noreply.github.com>
2020-09-29 11:34:14 +0900

    javascript_file作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,8 +6,26 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
+  belongs_to_active_hash :category
+  belongs_to_active_hash :state
+  belongs_to_active_hash :fee_delivery
+  belongs_to_active_hash :day_delivery
+
+
+
+  validates :prefecture, presence: true
+  validates :category, presence: true
+  validates :state, presence: true
+  validates :fee_delivery, presence: true
+  validates :day_delivery, presence: true
+
+
+  validates :prefecture_id, numericality: { other_than: 1 } 
+  validates :category_id, numericality: { other_than: 1 } 
+  validates :state_id, numericality: { other_than: 1 } 
+  validates :fee_delivery_id, numericality: { other_than: 1 } 
+  validates :day_delivery_id, numericality: { other_than: 1 } 
+
+
 
 end
-
-
-
---
+++ spec/models/day_delivery_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe DayDelivery, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
