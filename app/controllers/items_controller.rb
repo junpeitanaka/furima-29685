a416102dec7ce8d5f97eae6543a5d2ef33fb8eaf
junpeitanaka <69292110+junpeitanaka@users.noreply.github.com>
2020-10-02 20:47:56 +0900

    LGTMOK

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,7 +25,8 @@ class ItemsController < ApplicationController
   def destroy
   end
 
-  def order
+  def editor
+
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   
   get 'items', to: 'items#index'
 
-  resources :items, only: [:new, :create, :show, :destroy]
+  resources :items, only: [:new, :create, :show, :destroy, :edit]
 
 end
 
