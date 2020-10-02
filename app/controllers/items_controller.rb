fbb056b856cc212405e8a3e107d8122dd2845b0e
junpeitanaka <69292110+junpeitanaka@users.noreply.github.com>
2020-10-03 08:40:41 +0900

    First commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show, :new, ]
 
   def new
     @item = Item.new
@@ -25,7 +25,9 @@ class ItemsController < ApplicationController
   def destroy
   end
 
-  def order
+  def edit
+    @item = Item.find(params[:id])
+
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,5 @@ Rails.application.routes.draw do
   
   get 'items', to: 'items#index'
 
-  resources :items, only: [:new, :create, :show, :destroy]
-
+  resources :items, only: [:index, :new, :create, :destroy, :edit, :update, :show]
 end
-
-
