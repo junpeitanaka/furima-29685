7c56fda010d1400f9783347a2374672480add4d9
junpeitanaka <69292110+junpeitanaka@users.noreply.github.com>
2020-10-09 18:49:15 +0900

    commit

--- db/migrate/20201004051508_create_orders.rb
+++ db/migrate/20201004051508_create_orders.rb
@@ -1,7 +1,5 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
 
-  include ActiveModel::Model
-  attr_accessor :order_id,:postal_number, :prefecture_id, :city, :block, :building,:tele_number
 
   def change
     create_table :orders do |t|
